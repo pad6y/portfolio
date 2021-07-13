0f1267afb4e3a290abf641709bb2fdfb29423a1d
Patrick Wong <saihowong81@hotmail.com>
2021-07-13 20:14:52 +0100

    minor adjustments made to dropdown UI

--- resources/js/Pages/eCommerce/searchBarHeader.vue
+++ resources/js/Pages/eCommerce/searchBarHeader.vue
@@ -81,7 +81,7 @@
                 class="
                   grid grid-cols-7
                   mx-2
-                  pt-4
+                  pt-2
                   text-xs
                   border-t-2 border-gray-300
                   my-2
@@ -94,9 +94,9 @@
                 />
                 <div class="col-span-5 p-2">
                   <div class="grid grid-rows-2">
-                    <div class="pb-6">{{ item.product_name }}</div>
+                    <div class="pb-3">{{ item.product_name }}</div>
 
-                    <div class="flex flex-row">
+                    <div class="flex flex-row p-1">
                       <button @click.capture.stop="item.qty--">
                         <icons :name="'minus'" :class="'h-4 w-4'"></icons>
                       </button>
@@ -108,14 +108,14 @@
                       </button>
                     </div>
 
-                    <div class="grid grid-cols-2">
+                    <div class="grid grid-cols-2 mt-1">
                       <div class="flex items-end ml-2">
                         <button @click.capture.stop="removeFromCart(index)">
                           <icons :name="'bin'" :class="'h-4 w-4'"></icons>
                         </button>
                       </div>
                       <div class="flex justify-end items-end mt-2">
-                        £{{ item.product_price }}
+                        {{ formatCurrency(item.product_price) }}
                       </div>
                     </div>
                   </div>
@@ -123,7 +123,7 @@
               </div>
             </div>
             <div class="flex justify-end p-3 bg-gray-400 text-white">
-              Sub-Total: £{{ grandTotal }}
+              Sub-Total:{{ formatCurrency(grandTotal) }}
             </div>
             <div class="flex justify-end p-2">
               <gold-button
@@ -214,6 +214,13 @@ export default {
   emits: ["termChange", "removeItem"],
   inheritAttrs: false,
   methods: {
+    formatCurrency(price) {
+      // price = price / 100;
+      return price.toLocaleString("en-GB", {
+        style: "currency",
+        currency: "GBP",
+      });
+    },
     onInput: function (event) {
       this.$emit("termChange", event.target.value);
     },
